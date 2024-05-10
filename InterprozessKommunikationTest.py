abfffc7aa28eab5571656495719a6f4edaa04cf4
NBandura <niclasbandura@gmx.de>
2024-05-10 14:04:22 +0200

    IPC - Anpassung

--- InterprozessKommunikationTest.py
+++ InterprozessKommunikationTest.py
@@ -9,7 +9,7 @@ fortfahren=True
 
 while fortfahren:
     #Der letzte Prozess, der sich vom sharedMemory trennt muss den Speicher freigeben
-    eingabe=input("SharedMemory lesen (l) oder schreiben (s) Verbindung beenden (b) oder Speicher freigeben(f): ")
+    eingabe=input("SharedMemory lesen (l), schreiben (s) oder Speicher freigeben (f): ")
     if eingabe=="l":
         # Shared memory lesen
         shared_memory_mmap=mmap.mmap(shared_memory.fd, shared_memory.size,mmap.MAP_SHARED,mmap.PROT_READ)
@@ -24,13 +24,8 @@ while fortfahren:
         shared_memory_mmap=mmap.mmap(shared_memory.fd, shared_memory.size,mmap.MAP_SHARED,mmap.PROT_WRITE)
         shared_memory_mmap.write(text)
         shared_memory_mmap.close()
-    elif eingabe=="b":
-        # Shared memory Verbindung beenden
-        shared_memory.unlink()
-        fortfahren=False
     elif eingabe=="f":
-        # Shared memory freigeben
-        shared_memory.close_fd()
+        # Shared memory Verbindung beenden
         shared_memory.unlink()
         fortfahren=False
     else:
