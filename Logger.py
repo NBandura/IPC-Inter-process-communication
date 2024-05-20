31cade82f852d57ab1821fc3db6bb86fa36c42f0
Maurice Fleischer <maurice-fleischer@gmx.de>
2024-05-20 20:40:18 +0200

    Logger Methoden hinzugefügt

--- Logger.py
+++ Logger.py
@@ -4,12 +4,39 @@ import os #Importieren des OS Moduls, um Dateien zu erstellen und zu verwalten
 class Logger:
     
     def __init__(self, playerId): #Konstruktor für ein Logger Objekt
-        self.file = self.createFile(playerId)
+        self.playerId = playerId
+        self.file = self._logCreateFile()
 
-    def createFile(self, playerId): #Methode um Datei für Spieler zu erstellen
+    def _logCreateFile(self): #Methode um Datei für Spieler zu erstellen
         time = datetime.datetime.now()
-        file = open(time.strftime("%Y-%m-%d-%H-%M-%S") + f"-bingo-{playerId}.txt", "w")
-        
-        
+        filename = time.strftime("%Y-%m-%d-%H-%M-%S") + f"-bingo-{self.playerId}.txt"
+        file = open(filename, "w")
+        file.close
+        return filename
 
-        
+    def logGameStart(self):
+        time = datetime.datetime.now()
+        file = open(self.file, "a")
+        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Start des Spiels")
+        file.close
+
+    def logGameEnd(self):
+        time = datetime.datetime.now()
+        file = open(self.file, "a")
+        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Ende des Spiels")
+        file.close
+
+    def logCrossedWord(self, word, x, y):
+        time = datetime.datetime.now()
+        file = open(self.file, "a") 
+        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + word + f" ({x}/{y})")
+        file.close
+    
+    def logGameResult(self, result):
+        time = datetime.datetime.now()
+        file = open(self.file, "a")
+        if result == 0:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Sieg\n")
+        elif result == 1:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Abbruch\n")
+        file.close()
