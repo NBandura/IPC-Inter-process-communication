6bc12b41ac015e6e6b0ce4b08621ed28d5977583
Nima Turkmani <niamatullah.turkmani@gmail.com>
2024-05-24 10:36:19 +0200

    Update bingoGameV2.py
    IPC Implementierug

--- bingoGameV2.py
+++ bingoGameV2.py
@@ -5,29 +5,65 @@ from textual.widgets import Label, Footer, Button, Static
 import sys
 import os
 from Logger import Logger
+from IPC import SpielIPC
 
 import argparse
 
+
+
+
 logger = Logger(os.getpid()) #Logger für jeden Prozess erstellen
 logger.logGameStart()
+print()
+print()
+print()
+print()
+print()
+print("Bingo Game")
+spielname = input("Bitte geben Sie den Spielnamen ein, mit dem Sie sich verbinden wollen: ")
+IPC= SpielIPC(spielname) #IPC für das Spiel erstellen
 
-# ArgumentParser erstellen
-parser = argparse.ArgumentParser(description="Bingo-Spiel")
+if(IPC.checkIfStarted()):
+    size=IPC.getGroesse()
+    dateipfad=IPC.getDateipfad()
+    istStartProzess=False
+else:
+    size=0
+    while( size < 3 or size > 7 ):
+        size=int(input("Bitte gebe die Spielfeldgröße ein: "))
+        print(size)
+        if(size < 3 or size > 7 ):
+            print("Ungültige Größe: Erlaubt ist eine Größe von 3 - 7.")
+    
+    erfolgreich=False
+    while (not erfolgreich):
+        try:
+            dateipfad=input("Bitte gebe den Dateipfad ein: ")
+            test = []
+            with open(dateipfad, 'r') as file:
+                test = [zeile.strip() for zeile in file]
+            erfolgreich=True
+        except Exception:
+            print("Der Dateipfad ist ungültig!")
 
-# Argument hinzufügen
-parser.add_argument('-d', '--datei', type=str, required=True, help="Pfad zur Datei mit den Buzzwords")
-parser.add_argument('-s', '--size', type=int, required=True, help="Größe des Spielfelds")
-# Argumente parsen
-args = parser.parse_args()
+    
+    
+    IPC.setDateipfad(dateipfad)
+    IPC.setGroesse(size)
+    IPC.startGame()
+    istStartProzess=True
+    
+# Startprozess ist fertig
+# Noch zu machen:
+# Prozess muss solange laufen bis (IPC.checkIfBingo) true ist, dann müssen alle die es nicht gesendet haben, ein Verloren Feld bekommen
+# Und so lange auch, die Sachen dauerhaft aktualisieren:
+#   Wortliste anzeigen (IPC.getWortliste)
+#   Letztes Wort anzeigen (IPC.getLastWord)
+# Speicher Freigeben Button in "Spiel Abbrechen" umbennen
+# Sonst bei Bingo 5 sec. warten, dann speicher automatisch freigeben
 
-# Dateipfad aus den Argumenten extrahieren
-dateipfad = args.datei
-size = args.size
 
-# Überprüfen, ob die Größe des Spielfelds zwischen 3 und 7 liegt
-if size < 3 or size > 7:
-    print("Die Größe des Spielfelds muss zwischen 3 und 7 liegen.")
-    sys.exit(1)
+
 
 
 # Wörter werden eingelesen und in einer Liste gespeichert
@@ -36,23 +72,27 @@ with open(dateipfad, 'r') as file:
     buzzword_Wörter = [zeile.strip() for zeile in file]
 
 class Bingo(App):
-    CSS_PATH = "stylesMBW.tcss"
+    CSS_PATH = "StylesMBW.tcss"
     
     def compose(self):
         self.zufallswort_label = Label("", id="zufallswort_label")
         self.error_message = Label("", id="error_message")
         self.zufallswort_button = Button("Wort generieren", id="zufallswort")
-        self.clear_button = Button("Wort weg machen", id="clear")
         self.grid_container = Static(classes="bingo-grid")
         self.gewonnen_label = Label("", id="gewonnen_label")
         self.bingo_confirm_button = Button("Bingo bestätigen", id="bingo_confirm") 
         self.quit_button = Button("Quit", id="quit", classes="quit-button")  # Neuer Quit-Button
+        self.speicher_freigeben_button = Button("Speicher freigeben", id="speicher_freigeben", classes="freigeben")  # speicher freigeben
+        self.getWort = Label("", id="getWort", classes="getWort")
 
 
         yield self.error_message
-        yield self.zufallswort_label
-        yield self.zufallswort_button
-        yield self.clear_button
+        if(istStartProzess):
+            yield self.zufallswort_label
+            yield self.zufallswort_button
+            yield self.speicher_freigeben_button
+        else:
+            yield self.getWort
         yield self.grid_container
         yield self.gewonnen_label
         yield self.bingo_confirm_button  # Bingo-Bestätigungsbutton hinzufügen
@@ -136,6 +176,7 @@ class Bingo(App):
     @on(Button.Pressed, "#zufallswort")
     def zufallswort_generieren(self):
         zufallswort = random.choice(buzzword_Wörter)
+        IPC.addWord(zufallswort)
         self.zufallswort_label.update(zufallswort)
 
     @on(Button.Pressed, "#bingo_confirm")  # Event-Handler für den Bingo-Bestätigungsbutton
@@ -146,19 +187,31 @@ class Bingo(App):
             #Message für Bingo gewonnen nach dem Button gedrückt wurde 
         else:
             self.update_gewonnen_label("Noch kein Bingo. Versuche es weiter!")
-
-    @on(Button.Pressed, "#clear")
-    def clear_label(self):
-        self.zufallswort_label.update("")
-        self.error_message.update("")
     
     def update_gewonnen_label(self, message):
         self.gewonnen_label.update(message)
 
+    def update_lastWort(self, message):
+        self.getWort.update(message)
+
+    # Versuch mit der Aktualisierung
+    #def on_mount(self):
+    #    (
+    #        1/1,
+    #        self.update_lastWort(IPC.getWord())
+    #       pause=True
+    #    )
+    
     @on(Button.Pressed, "#quit")  # Event-Handler für den Quit-Button
     def on_quit(self, event):
         logger.logGameEnd()
         self.exit()
 
-if __name__ == "__main__":
+    @on(Button.Pressed, "#speicher_freigeben") 
+    def speicher_freigeben(self):
+        IPC.speicherFreigeben() # Event-Handler für den Speicher freigeben-Button
+
+    
+
+if __name__ == "__main__": 
     Bingo().run()
