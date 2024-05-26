bdd4856e4e0897c044aab2f9cccdb8719871739b
Maurice Fleischer <maurice-fleischer@gmx.de>
2024-05-26 18:29:56 +0200

    Logger Korrekturen

--- Logger.py
+++ Logger.py
@@ -1,54 +1,52 @@
-import datetime #Importieren des Time moduls zum erfassen der Zeit
-import os #Importieren des OS Moduls, um Dateien zu erstellen und zu verwalten
+import datetime  # Importieren des datetime Moduls zum Erfassen der Zeit
+import os  # Importieren des os Moduls, um Dateien zu erstellen und zu verwalten
 
 class Logger:
     
-    def __init__(self, playerId): #Konstruktor für ein Logger Objekt
+    def __init__(self, playerId):
         self.playerId = playerId
         self.file = self._logCreateFile()
 
-    def _logCreateFile(self): #Methode um Datei für Spieler zu erstellen
+    def _logCreateFile(self):
+        logsFolderName = "logs"
+        if not os.path.exists(logsFolderName):
+            os.makedirs(logsFolderName)
+        
         time = datetime.datetime.now()
-        filename = time.strftime("%Y-%m-%d-%H-%M-%S") + f"-bingo-{self.playerId}.txt"
-        file = open(filename, "w")
-        file.close
+        filename = time.strftime(f"{logsFolderName}/%Y-%m-%d-%H-%M-%S") + f"-bingo-{self.playerId}.txt"
+        with open(filename, "w") as file:
+            pass
         return filename
 
     def logGameStart(self):
         time = datetime.datetime.now()
-        file = open(self.file, "a")
-        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Start des Spiels\n")
-        file.close
+        with open(self.file, "a") as file:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Start des Spiels\n")
 
     def logGameEnd(self):
         time = datetime.datetime.now()
-        file = open(self.file, "a")
-        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Ende des Spiels\n")
-        file.close
+        with open(self.file, "a") as file:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Ende des Spiels\n")
 
     def logCrossedWord(self, word, buttonId):
         time = datetime.datetime.now()
-        file = open(self.file, "a") 
-        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + word + " (crossed) " + buttonId + "\n")
-        file.close
+        with open(self.file, "a") as file:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + word + " (crossed) " + buttonId + "\n")
 
     def logUncrossedWord(self, word, buttonId):
         time = datetime.datetime.now()
-        file = open(self.file, "a") 
-        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + word + " (uncrossed) " + buttonId + "\n")
-        file.close
+        with open(self.file, "a") as file:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + word + " (uncrossed) " + buttonId + "\n")
 
     def logBingoOpportunity(self):
         time = datetime.datetime.now()
-        file = open(self.file, "a")
-        file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Bingo möglich\n")
-        file.close
-    
+        with open(self.file, "a") as file:
+            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Bingo möglich\n")
+
     def logGameResult(self, result):
         time = datetime.datetime.now()
-        file = open(self.file, "a")
-        if result == 0:
-            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Sieg\n")
-        elif result == 1:
-            file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Abbruch\n")
-        file.close()
+        with open(self.file, "a") as file:
+            if result == 0:
+                file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Sieg\n")
+            elif result == 1:
+                file.write(time.strftime("%Y-%m-%d-%H-%M-%S ") + "Abbruch\n")
