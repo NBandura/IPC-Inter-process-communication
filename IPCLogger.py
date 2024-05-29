c0e4d424c0488798f542428b6a9267659ccb0118
Maurice Fleischer <maurice-fleischer@gmx.de>
2024-05-29 12:55:44 +0200

    Hinzufügen der GameID zum IPC Logger

--- IPCLogger.py
+++ IPCLogger.py
@@ -3,8 +3,9 @@ import os  #Importieren des os Moduls, um Dateien zu erstellen und zu verwalten
 
 class IPCLogger:
     
-    def __init__(self, gameName):
+    def __init__(self, gameName, gameId):
         self.gameName = gameName
+        self.gameId = gameId
         self.file = self._logCreateFile() #automatisches Erstellen der Log-Datei beim erstellen eines Logger Objektes
 
     def _logCreateFile(self): 
@@ -12,7 +13,7 @@ class IPCLogger:
         if not os.path.exists(logsFolderName): #Erstellen eines neuen Log-Ordners, falls dieser nicht vorhanden ist
             os.makedirs(logsFolderName)
         
-        filename = f"{logsFolderName}/ipc-{self.gameName}.txt"
+        filename = f"{logsFolderName}/ipc-{self.gameName}-{self.gameId}.txt"
         with open(filename, "w") as file: #"with open" um Datei nicht wieder schließen zu müssen
             pass #pass um Datei "nur" zu erstellen
         return filename #Rückgabe des Dateinamens um ihn im Objekt zu verwenden
